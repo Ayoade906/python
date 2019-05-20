38c1699edea72f1f3b6224bb29a214ceb909c1eb
Ayoade Oriola <ayoriola@gmail.com>
2019-05-20 23:18:04 +0100

    Refactor AP Sum to Function
    1. Wrote a new function to compute the summation.
    
    Signed-off-by: Ayoade Oriola <ayoriola@gmail.com>

--- oneto100.py
+++ oneto100.py
@@ -9,7 +9,13 @@ for iota in range(n):
 print(f'for_method = {for_method}')
 print(f'for_method took {time.time() - startTime} second !')
 
+
+def sum_n(num):
+    return num * (num + 1) * 0.5
+
+
 startTime = time.time()
-sap_method = n * (n + 1) * 0.5
+sap_method = sum_n(n)
 print(f'sap_method = {sap_method}')
 print(f'sap_method took {time.time() - startTime} second !')
+
